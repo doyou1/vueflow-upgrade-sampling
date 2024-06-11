da67c3fd254c328cc73643de707abd7e781a5a6f
doyou1 <tla149@daum.net>
2024-06-11 20:32:09 +0900

    layout init

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  Hello World
+  <el-container class="wrap conatiner">
+    <el-header class="header">Header</el-header>
+    <el-container class="container">
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
+<script setup lang="ts">
+import { ElContainer, ElHeader, ElAside, ElMain } from "element-plus";
+</script>
+
 <style scoped lang="scss">
+.wrap {
+  width: 100%;
+  height: calc(100vh - 2rem);
+}
+.container {
+  background-color: goldenrod;
+}
+.header {
+  background-color: skyblue;
+}
+.aside {
+  background-color: yellow;
+}
+.main {
+  background-color: green;
+}
+</style>
+
+<style lang="scss">
 /** vueflow css */
 @import "https://cdn.jsdelivr.net/npm/@vue-flow/core@1.24.1/dist/style.css";
 @import "https://cdn.jsdelivr.net/npm/@vue-flow/core@1.24.1/dist/theme-default.css";
 @import "https://cdn.jsdelivr.net/npm/@vue-flow/controls@latest/dist/style.css";
 @import "https://cdn.jsdelivr.net/npm/@vue-flow/minimap@latest/dist/style.css";
 @import "https://cdn.jsdelivr.net/npm/@vue-flow/node-resizer@latest/dist/style.css";
-
 </style>
