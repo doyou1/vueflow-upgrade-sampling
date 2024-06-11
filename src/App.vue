40cb9328f8909f80f6a545cb2c078c2b4d708914
doyou1 <tla149@daum.net>
2024-06-11 20:25:14 +0900

    project init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  Hello World
+</template>
+
+<style scoped lang="scss">
+/** vueflow css */
+@import "https://cdn.jsdelivr.net/npm/@vue-flow/core@1.24.1/dist/style.css";
+@import "https://cdn.jsdelivr.net/npm/@vue-flow/core@1.24.1/dist/theme-default.css";
+@import "https://cdn.jsdelivr.net/npm/@vue-flow/controls@latest/dist/style.css";
+@import "https://cdn.jsdelivr.net/npm/@vue-flow/minimap@latest/dist/style.css";
+@import "https://cdn.jsdelivr.net/npm/@vue-flow/node-resizer@latest/dist/style.css";
+
+</style>
